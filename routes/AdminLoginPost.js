873cc2b03832e3897aa81f502eddaf8410299217
luben <lubenfrank48@gmail.com>
2019-04-17 21:03:22 +0530

    landing page update

--- routes/AdminLoginPost.js
+++ routes/AdminLoginPost.js
@@ -1,27 +1,24 @@
 "use strict";
 
 var Admin = function(request, response) {
-   var DB =request.app.locals.DB;
+  var DB = request.app.locals.DB;
 
-   var email = request.body.email;
-    var password  = request.body.password;
-   
+  var email = request.body.email;
+  var password = request.body.password;
 
-   DB.collection("AdminLogin").findOne({email: email} ,function(error, data){
-       console.log(data);
-      if(error){
-          console.log("invalid mail id");
-          return response.render("AdminLogin.hbs");
-      }
-      else if(data.password == password){
-          console.log("invalid password");
-          return response.render("AdminLogin.hbs")
-      }
-      else {
-          console.log(data);
-          return response.render("AdminDashboard.hbs");
-      }
-   })
-}
+  DB.collection("AdminLogin").findOne({ email: email }, function(error, data) {
+    console.log(data);
+    if (error) {
+      console.log("invalid mail id");
+      return response.render("AdminLogin.hbs");
+    } else if (data.password == password) {
+      console.log("correct password");
+      return response.render("AdminLogin.hbs");
+    } else {
+      console.log(data);
+      return response.render("AdminDashboard.hbs");
+    }
+  });
+};
 
 exports.Admin = Admin;
--- routes/landingPage.js
+++ routes/landingPage.js
@@ -1,6 +1,20 @@
 "use strict";
 
 var landingPage = function(req, res) {
-  res.render("landingPage.hbs");
+  var DB = req.app.locals.DB;
+
+  DB.collection("videos")
+    .find({})
+    .toArray(function(error, data) {
+      if (error) {
+        console.log("couldnt upload db error");
+      } else {
+        var videoArrayOfTwo = [];
+        for (var i = 0; i < data.length; i += 2) {
+          videoArrayOfTwo.push(data.slice(i, i + 2));
+        }
+       return  res.render("landingPage.hbs", { videoArrayOfTwo: videoArrayOfTwo });
+      }
+    });
 };
 exports.landingPage = landingPage;
