3418019490463711a2e972fda466186ae62f7178
luben <lubenfrank48@gmail.com>
2019-04-24 14:45:49 +0530

    working app

--- routes/logout.js
+++ routes/logout.js
@@ -3,7 +3,7 @@
 var logout = function(request, response) {
   request.session.user = null;
   request.session.admin = null;
-  response.redirect("/");
+  response.redirect("/login");
 };
 
 exports.logout = logout;
