7df184c25e8b2ee8b77d4e7e1a3d621cf6310ba5
luben <lubenfrank48@gmail.com>
2019-04-26 15:52:58 +0530

    test deploment

--- routes/uploadPost.js
+++ routes/uploadPost.js
@@ -14,7 +14,7 @@ var uploadPost = function(req, res) {
   fileUpload.parse(req, function(error, fields, files) {
     var data = {};
     console.log(fields);
-    data.title = fields.title[0];
+    data.title = fields.title;
     data.description = fields.description[0];
     data.originalFileName = files.image[0].originalFilename;
     data.path = files.image[0].path;
